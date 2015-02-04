cf3f6e5a5deae258b225386a5ac8726517be9b3b
nowgoant <631359926@qq.com>
2015-02-04 11:35:20 +0800

    init

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  require('time-grunt')(grunt);
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Configurable paths
+  var config = {
+    app: 'app',
+    dist: 'dist'
+  };
+
+  grunt.initConfig({
+    config: config,
+    // 自动雪碧图
+    // sprite: {
+    //   allslice: {
+    //     files: [{
+    //       //启用动态扩展
+    //       expand: true,
+    //       // css文件源的文件夹
+    //       cwd: '<%= config.app %>/styles/',
+    //       // 匹配规则
+    //       src: ['*.css'],
+    //       //导出css和sprite的路径地址
+    //       dest: 'tmp/',
+    //       // 导出的css名
+    //       ext: '.sprite.css'
+    //     }],
+    //     options: {
+    //       // 默认使用GM图像处理引擎
+    //       'engine': 'gm',
+    //       // 默认使用二叉树最优排列算法
+    //       'algorithm': 'binary-tree',
+    //       // 给雪碧图追加时间戳，默认不追加
+    //       'imagestamp': false,
+    //       // 给样式文件追加时间戳，默认不追加
+    //       'cssstamp': true,
+    //       // 是否以时间戳为文件名生成新的雪碧图文件，默认不生成新文件
+    //       'newsprite': true
+    //     }
+    //   }
+    // },
+    sprite: {
+      // options: {
+      //   // cwd: '<%= config.app %>/images/sprites/'
+      // },
+
+      all: {
+        src: '<%= config.app %>/images/*.png',
+        dest: '<%= config.dist %>/images/spritesheet.png',
+        destCss: '<%= config.dist %>/styles/sprites.css'
+      },
+      home: {
+        src: '<%= config.app %>/images/sprites/home/*.png',
+        dest: '<%= config.dist %>/images/sprites/home/spritesheet.png',
+        destCss: '<%= config.dist %>/styles/home/sprites/sprites.css'
+      }
+    }
+  });
+};
